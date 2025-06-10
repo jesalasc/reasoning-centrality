c88036e365235e1594d2374953ba90e07931f579
canon-robins <crobins@mit.edu>
2025-06-10 15:42:06 -0400

    Update generate_weights.py

--- generate_weights.py
+++ generate_weights.py
@@ -1,23 +1,35 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import matplotlib.pyplot as plt
+import numpy as np
 
 device = "mps" if torch.backends.mps.is_available() else "cpu"
-path = "/Users/canonrobins/Documents/GitHub/DeepSeek-R1-Distill-Qwen-1.5B"
+path = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 
-model = AutoModelForCausalLM.from_pretrained(path, attn_implementation="eager").to(device)
-tokenizer = AutoTokenizer.from_pretrained(path)
+model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True, attn_implementation="eager").to(device)
+tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
 
-inputs = tokenizer("Given that 'a' equals 2 and 'b' equals 3, Calculate 'a' multiplied by 'b', give an exact numerical computation. Your answer should just be a one digit integer", return_tensors="pt")
+inputs = tokenizer("Calculate 13 * 2. Return your answer as an integer.", return_tensors="pt")
 inputs = {k: v.to(model.device) for k, v in inputs.items()}
 outputs = model(**inputs, output_attentions=True)
 output_attentions = outputs.attentions
 
-def print_weights(attentions, layer, batch_i, head_i):
+def print_weights(attentions, layer=0, batch_i=0, head_i=0):
     print(f"Num layers: {len(attentions)}")
     print(f"Shape vector at layer: {attentions[layer].shape}")
     print(f"Layer {layer}, Batch {batch_i}, Head {head_i}: {attentions[layer][batch_i,head_i]}")
 
-print_weights(output_attentions, 0, 0, 0)
+def plot_weights(attentions, layer=0, batch_i=0, head_i=0):
+    matrix = attentions[layer][batch_i,head_i]
+    flattened_vals = matrix.flatten().detach().cpu().numpy()
+    log_transformed = np.log(flattened_vals + 1)
+    plt.hist(log_transformed, bins=75)
+    plt.xlabel("Weight")
+    plt.ylabel("Frequency")
+    plt.grid(True)
+    plt.show()
 
-generated = model.generate(**inputs, max_new_tokens=10000)
+print_weights(output_attentions)
+generated = model.generate(**inputs, max_new_tokens=40)
 print(f"Output: {tokenizer.decode(generated[0])}")
+plot_weights(output_attentions)
