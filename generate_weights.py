fixed masking

generate_weights.py
@@ -10,7 +10,7 @@ path = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True, attn_implementation="eager").to(device)
 tokenizer = AutoTokenizer.from_pretrained(path, trust_remote_code=True)
 
-inputs = tokenizer("Question: What is 2 + 2? Answer:", return_tensors="pt")
+inputs = tokenizer("Question: Let's say a = 2 and b =4. What is a * b * a? Answer:", return_tensors="pt")
 inputs = {k: v.to(model.device) for k, v in inputs.items()}
 outputs = model(**inputs, output_attentions=True)
 output_attentions = outputs.attentions
@@ -22,35 +22,40 @@ def print_weights(attentions, layer=0, batch_i=0, head_i=0):
 
 def plot_weights(attentions, layer=0, batch_i=0, head_i=0):
     # creating histogram
-    matrix = attentions[layer][batch_i,head_i]
-    flattened_vals = matrix.flatten().detach().cpu().numpy()
+    layer_matrix = attentions[layer][batch_i]
+    flattened_vals = layer_matrix.flatten().detach().cpu().numpy()
+    bin_num = int(len(flattened_vals ** 0.5))
+    print(f"bin_num: {bin_num}")
     log_weights = np.log(flattened_vals + 1)
-    plt.hist(log_weights, bins=75)
+    plt.hist(log_weights, bins=bin_num)
     plt.xlabel("Ln Transformed Weight")
     plt.ylabel("Frequency")
     plt.grid(True)
     plt.show()
 
     # creating masked relational graph
-    total_weights = attentions[layer].shape[2] * attentions[layer].shape[3]
-    threshold = 1/2 * total_weights
+    total_weights = flattened_vals.shape[0]
+    threshold = 1/10 * total_weights
 
-    hist, bin_edges = np.histogram(log_weights, bins=1000)
+    hist, bin_edges = np.histogram(log_weights, bins=bin_num)
     low_count_bins = np.where(hist < threshold)[0]
-    bad_ranges = set()
+    low_ranges = set()
     for i in low_count_bins:
-        bad_ranges.add((bin_edges[i], bin_edges[i+1]))
+        low_ranges.add((bin_edges[i], bin_edges[i+1]))
 
-    matrix_np = matrix.detach().cpu().numpy()
-    log_matrix = np.log(matrix_np + 1)
-    mask = np.zeros_like(matrix_np, dtype=bool)
+    head_matrix = attentions[layer][batch_i, head_i]
+    head_matrix_np = head_matrix.detach().cpu().numpy()
+    log_matrix = np.log(head_matrix_np + 1)
+    mask = np.zeros_like(head_matrix_np, dtype=bool)
 
-    for low, high in bad_ranges:
-        mask |= (log_matrix >= low) & (log_matrix < high)
+    for i, (low, high) in enumerate(low_ranges):
+        if high == bin_edges[-1]:
+            mask |= (log_matrix >= low) & (log_matrix <= high)
+        else:
+            mask |= (log_matrix >= low) & (log_matrix < high)
 
-    true_mask = ~mask
-    print(true_mask)
-    seq_len = matrix.shape[0]
+    print(mask)
+    seq_len = head_matrix.shape[0]
     G = nx.DiGraph()
 
     for i in range(seq_len):
@@ -58,19 +63,19 @@ def plot_weights(attentions, layer=0, batch_i=0, head_i=0):
 
     for i in range(seq_len):
         for j in range(seq_len):
-            if true_mask[i, j]:
-                weight = matrix_np[i, j]
+            if mask[i, j]:
+                weight = head_matrix_np[i, j]
                 G.add_edge(i, j, weight=weight)
 
     pos = nx.circular_layout(G)
     nx.draw(G, pos, with_labels=True)
     labels = nx.get_edge_attributes(G, 'weight')
-    nx.draw_networkx_edge_labels(G, pos, edge_labels={k: v for k, v in labels.items()})
+    nx.draw_networkx_edge_labels(G, pos, edge_labels={k: f"{v:.4e}" for k, v in labels.items()}, font_size=5)
     plt.title(f"Relational Graph at Layer {layer} and Head {head_i}")
     plt.show()
 
 
-print_weights(output_attentions)
-# generated = model.generate(**inputs, max_new_tokens=150)
-# print(f"Output: {tokenizer.decode(generated[0])}")
+# print_weights(output_attentions)
+generated = model.generate(**inputs, max_new_tokens=150)
+print(f"Output: {tokenizer.decode(generated[0])}")
 plot_weights(output_attentions)